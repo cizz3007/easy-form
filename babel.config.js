dc53fd1e8c6548aa21cb3fcef56e9f580fd16ab7
kimchanghyeon <changhyeonk0508@nexon.co.kr>
2022-04-08 11:44:14 +0900

    update:babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 const BABEL_ENV = process.env.BABEL_ENV;
 const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs';
-const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm';
 
 module.exports = function (api) {
   api.cache(true);
@@ -10,10 +9,7 @@ module.exports = function (api) {
       '@babel/preset-env',
       {
         loose: true,
-        modules: isCommonJS ? 'commonjs' : false,
-        targets: {
-          esmodules: isESM ? true : undefined
-        }
+        modules: isCommonJS ? 'commonjs' : false
       }
     ],
     '@babel/preset-typescript',
